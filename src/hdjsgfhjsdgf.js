0be0aaacbab33e6e8b6a80ab3c0f51da64421678
Your Name <you@example.com>
2019-08-11 21:39:20 -0400

    Done.

---
+++ src/hdjsgfhjsdgf.js
@@ -0,0 +1,29 @@
+
+// ---------------------------------Constants
+
+
+
+// ---------------------------Fetches
+fetch(`http://localhost:3000/toys/${toyData.id}`)
+    .then(response => response.json())
+    .then(json => slapondom(json))
+
+
+
+
+
+
+
+
+
+
+
+// -------------------------------------------SlapToDom
+
+
+
+
+
+
+
+// ------------------------------------------EventListneers
